addd61b34af4e1ff2221c8591930bc8a0a7223cc
Maria Martinez <marmareg82@gmail.com>
2023-01-10 14:24:05 +0100

    ejercicio 2 js

---
+++ javaScript/ejercicio_02/ejeccio_2.js
@@ -0,0 +1,15 @@
+/* Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+- Tu nombre (string)
+
+- Tu edad (number)
+
+- ¿Eres desarrollador? (boolean)
+
+- Tu fecha de nacimiento (Date)
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+*/
+
+const sobreMi = ["Maria", 40, true, "1982-08-10", "Los renglones torcidos de Dios"];
+console.log(sobreMi)
