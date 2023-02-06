d5d526489beaf41e49060f7a2d2b7a2ee1aa0f33
Maria Martinez <marmareg82@gmail.com>
2023-02-06 11:49:53 +0100

    ejercicio 4 js

---
+++ javaScript/ejercicio_04/ejercicio_4.js
@@ -0,0 +1,17 @@
+
+let nombre = "María";
+let apellido = "Martínez";
+let estudiante = nombre.concat(" ",apellido)
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLocaleLowerCase();
+let contar = estudiante.length;
+let primeraLetra = nombre.charAt(0);
+let ultimaLetra = apellido.charAt(apellido.length - 1);
+let eliminarEspacios = estudiante.trim();
+let bool = estudiante.includes(nombre);
+
+
+
+
+
+
